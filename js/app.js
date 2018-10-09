673610f5e698e71bf3d4459a24143a80b85a92ea
Jacob Anderson <1anderson2jacob@gmail.com>
2018-10-09 14:45:29 -0700

    Question 1 works and structure made for the rest of the questions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,51 @@
 'use strict';
+var numCorrect = 0;
+alert('Welcome! Play my guessing game');
 
-alert('oi');
+var username = prompt('What is your name?');
+
+alert('Welcome, ' + username + '! Let\'s get started!');
+
+
+var answerOne = prompt('Do I have more than 3 pets?').toLowerCase();
+
+if (answerOne === 'no'|| answerOne === 'n'){
+    alert('Correct, I only have 2 pets');
+    console.log('The user got Question 1 correct');
+    numCorrect++;
+} else {
+    alert('Wrong, I only have 2 pets');
+    console.log('The user got Question 1 wrong');
+    //tell them
+    //console.log the result
+}
+
+var answerTwo = prompt('Do I like to play cards?').toLowerCase();
+
+if (answerTwo === 'yes' || answerTwo === 'y'){
+
+} else {
+
+}
+
+var answerThree = prompt('').toLowerCase();
+if (){
+
+} else {
+
+}
+
+var answerFour = prompt('').toLowerCase();
+if (){
+
+} else {
+    
+}
+var answerFive = prompt('').toLowerCase();
+if (){
+
+} else {
+    
+}
+
+alert('You got ' + numCorrect + ' answers correct!');
